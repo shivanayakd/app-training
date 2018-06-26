61019b57208b0125dc23831413d56839644993f9
ADHARMAN\sdharavath <shiva.dharavath@harman.com>
2018-06-26 21:49:28 +0530

    components classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent} from './nav/navigation.component';
+import { MainComponent } from './main/main.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
